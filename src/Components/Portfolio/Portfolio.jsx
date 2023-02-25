aa6922d94903c7a142e8cc126879075cc13d928f
Rabins <rabinsneupane07@gmail.com>
2023-02-25 16:00:54 +0530

    added some projects & deleted some photos

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -5,6 +5,9 @@ import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { themeContext } from "../../Context";
 import fnThumbnail from "../../img/fashionninza.png";
+import findMoviezThumbnail from "../../img/findMoviez.png";
+import furnitureThumbnail from "../../img/furniture.png";
+import gpt3Thumbnail from "../../img/gpt3.png";
 import IMDbThumbnail from "../../img/imdbClone.png";
 import LorukiThumbnail from "../../img/loruki.png";
 import ManageThumbnail from "../../img/manage.png";
@@ -94,6 +97,39 @@ const Portfolio = () => {
               rel={"noreferrer"}
             >
               <img src={ManageThumbnail} alt="" />
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="portfolio">
+            <a
+              href="https://homeyfurniture.netlify.app/"
+              target={"_blank"}
+              rel={"noreferrer"}
+            >
+              <img src={furnitureThumbnail} alt="" />
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="portfolio">
+            <a
+              href="https://findmoviez.netlify.app"
+              target={"_blank"}
+              rel={"noreferrer"}
+            >
+              <img src={findMoviezThumbnail} alt="" />
+            </a>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="portfolio">
+            <a
+              href="https://chatgpt3openai.netlify.app"
+              target={"_blank"}
+              rel={"noreferrer"}
+            >
+              <img src={gpt3Thumbnail} alt="" />
             </a>
           </div>
         </SwiperSlide>
